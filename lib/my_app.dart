aaeab1cc987b2e09250e20b690fcb5a77986d3ae
Sahil pillania <sahilpillania88@gmail.com>
2024-02-29 20:20:42 +0530

    Added new page

---
+++ lib/my_app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key, required this.client, required this.channel});
+
+  final StreamChatClient client;
+  final Channel channel;
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Chat Application',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        // home: const MyHomePage(title: 'Chat App'),
+        builder: (context, child) {
+          return StreamChat(client: client, child: child);
+        });
+  }
+}
